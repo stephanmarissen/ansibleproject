046c72549464eb243908b884b06333e25d0105cd
Stephan Marissen <stephanrmarissen@gmail.com>
2019-01-28 11:35:25 +0100

    First commit

---
+++ hostfolder/tera/gold/roles/webserver/files/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<style>
+table, th, td{
+border: 1px solid black;
+}
+</style>
+</head>
+<body>
+<h1>
+Hosted On Server :
+<?php
+echo $_SERVER['SERVER_ADDR'];
+?>
+</h1>
+<?php
+$servername = "172.16.10.20";
+$username = "user";
+$password = "password";
+$dbname = "testdb";
+
+// creeer connectie
+$conn = new mysqli($servername,$username,$password,$dbname);
+// check connectie
+if ($conn->connect_error) {
+die("Connection failed:".$conn->connect_error);
+}
+
+$sql = "SELECT id,first_name,last_name FROM authors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+echo "<table><tr><th>ID</th><th>Naam</th></tr>";
+
+        //data output rows
+  while($row = $result->fetch_assoc()) {
+   echo "<tr><td>" . $row["id"]. "</td><td>" . $row["first_name"]. "" . $row["last_name"]. "</td></tr>";
+  }
+  echo "</table>";
+} else {
+echo "0 results";
+}
+$conn->close();
+?>
+</body>
+</html>
